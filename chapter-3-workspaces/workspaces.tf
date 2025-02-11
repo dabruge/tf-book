154f23e1c1024850aae507ffcaba6c4428d5da3b
dabruge <davidbrucegeddes@gmail.com>
2025-02-11 16:53:33 +0000

    Chapter 3 remote state and workspaces

---
+++ chapter-3-workspaces/workspaces.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "terraform-up-and-running-state-2025-02-11"
+    key    = "workspaces-example/terraform.tfstate"
+    region = "us-east-2"
+    
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+}
